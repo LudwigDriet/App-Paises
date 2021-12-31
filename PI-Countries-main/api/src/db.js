cc49f79e56b04272ce68bc0b4643bc07028e8e21
LudwigDriet <ludwigmisael95@gmail.com>
2021-12-31 11:45:43 -0300

    deploy

--- PI-Countries-main/api/src/db.js
+++ PI-Countries-main/api/src/db.js
@@ -2,9 +2,32 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST , DB__NAME } = process.env;
 
-const sequelize = new Sequelize(
+const sequelize =
+process.env.NODE_ENV === 'production'
+? new Sequelize({
+database:DB__NAME,
+dialect: 'postgres',
+host: DB_HOST,
+port: 5432,
+username: DB_USER,
+password: DB_PASSWORD,
+pool:{
+  max:3,
+  min: 1,
+  idle:10000,
+},
+dialectOptions:{
+  ssl:{
+    require: true,
+    rejectUnauthorized: false,
+  },
+  keepAlive: true,
+},
+ssl: true,
+})
+ : new Sequelize(
   `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`,
   {
     logging: false, // set to console.log to see the raw SQL queries
